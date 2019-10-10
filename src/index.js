fff2d1c470938378981aac845cb9f56673a8a3ac
Svetlana Kurbacheva <svetlana.a.k.15@mail.ru>
2019-10-10 19:00:06 +0300

    multiply

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function multiply(first, second) {
-  // your solution
+    var a = first.split('').reverse();
+    var b = second.split('').reverse();
+
+    var stack = [];
+
+    for (var i = 0; i < a.length; i++) {
+        for (var j = 0; j < b.length; j++) {
+            var m = a[i] * b[j];
+            stack[i + j] = (stack[i + j]) ? stack[i + j] + m : m;
+        }
+    }
+
+    for (var i = 0; i < stack.length; i++) {
+        var num = stack[i] % 10;
+        var move = Math.floor(stack[i] / 10);
+        stack[i] = num;
+
+        if (stack[i + 1])
+            stack[i + 1] += move;
+        else if (move != 0)
+            stack[i + 1] = move;
+    }
+
+
+    return stack.reverse().join('');
 }
